8d841fa09ddba648165809f719322005eb180d24
ycssona <ycssona@gmail.com>
2024-01-30 22:10:13 +0530

    Creating DTOs inside accounts microservice

---
+++ accounts/src/main/java/com/theonewhocode/accounts/dto/AccountsDto.java
@@ -0,0 +1,13 @@
+package com.theonewhocode.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountsDto {
+
+    private Long accountNumber;
+
+    private String accountType;
+
+    private String branchAddress;
+}
---
+++ accounts/src/main/java/com/theonewhocode/accounts/dto/CustomerDto.java
@@ -0,0 +1,13 @@
+package com.theonewhocode.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class CustomerDto {
+
+    private String name;
+
+    private String email;
+
+    private String mobileNumber;
+}
