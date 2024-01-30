7faf1c3ed200d1dc260033871aa424641a02db5f
ycssona <ycssona@gmail.com>
2024-01-30 18:47:43 +0530

    Creating Hello Worlds REST API

---
+++ accounts/src/main/java/com/theonewhocode/accounts/controller/AccountsController.java
@@ -0,0 +1,13 @@
+package com.theonewhocode.accounts.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountsController {
+
+    @GetMapping("sayHello")
+    public String sayHello() {
+        return "Hi World!";
+    }
+}
