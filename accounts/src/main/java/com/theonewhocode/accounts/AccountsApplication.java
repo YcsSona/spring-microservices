acaef2a30a3f3c124c326ac7d3aa878823856259
ycssona <ycssona@gmail.com>
2024-02-01 22:40:21 +0530

    Enhancing documentation of REST APIs using @OpenAPIDefinition

--- accounts/src/main/java/com/theonewhocode/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/theonewhocode/accounts/AccountsApplication.java
@@ -1,11 +1,36 @@
 package com.theonewhocode.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAuditImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "EazyBank Accounts microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Sample User",
+                        email = "email@email.com",
+                        url = "https://www.google.com/"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.google.com/"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EazyBank Accounts microservice REST API Documentation",
+                url = "https://www.google.com/"
+        )
+)
 public class AccountsApplication {
 
     public static void main(String[] args) {
