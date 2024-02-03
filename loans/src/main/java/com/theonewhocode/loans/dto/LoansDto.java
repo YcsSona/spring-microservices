c42ff1bd10f4f3d7c252f418219fe7c5918717f4
ycssona <ycssona@gmail.com>
2024-02-03 23:25:37 +0530

    Creating DTOs inside loans microservice

---
+++ loans/src/main/java/com/theonewhocode/loans/dto/LoansDto.java
@@ -0,0 +1,15 @@
+package com.theonewhocode.loans.dto;
+
+public class LoansDto {
+    private String mobileNumber;
+
+    private String loanNumber;
+
+    private String loanType;
+
+    private int totalLoan;
+
+    private int amountPaid;
+
+    private int outstandingAmount;
+}
