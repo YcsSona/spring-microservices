5de08fe328d74cea099c3996145083097c7de3e0
ycssona <ycssona@gmail.com>
2024-02-04 13:12:46 +0530

    Enhancing documentation of REST APIs of loans microservice using @OpenAPIDefinition

--- loans/src/main/java/com/theonewhocode/loans/LoansApplication.java
+++ loans/src/main/java/com/theonewhocode/loans/LoansApplication.java
@@ -1,9 +1,34 @@
 package com.theonewhocode.loans;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Loans microservice REST API Documentation",
+                description = "EazyBank Loans microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Sample User",
+                        email = "email@email.com",
+                        url = "https://www.google.com/"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.google.com/"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EazyBank Loans microservice REST API Documentation",
+                url = "https://www.google.com/"
+        )
+)
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class LoansApplication {
