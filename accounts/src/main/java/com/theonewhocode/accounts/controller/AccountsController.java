148060d690e08bdd9ab716744070f4c16057d651
ycssona <ycssona@gmail.com>
2024-02-12 17:24:53 +0530

    Reading configurations using Enviroment interface inside Accounts microservice

--- accounts/src/main/java/com/theonewhocode/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/theonewhocode/accounts/controller/AccountsController.java
@@ -15,6 +15,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -32,6 +33,9 @@ public class AccountsController {
 
     @Autowired
     private IAccountsService accountsService;
+
+    @Autowired
+    private Environment environment;
 
     @Value("${build.version}")
     private String buildVersion;
@@ -195,5 +199,31 @@ public class AccountsController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(buildVersion);
+    }
+
+    @Operation(
+            summary = "Get Java version",
+            description = "Get Java version details that is installed into accounts microservice"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(
+                                    implementation = ErrorResponseDto.class
+                            )
+                    )
+            )
+    })
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(environment.getProperty("JAVA_HOME"));
     }
 }
