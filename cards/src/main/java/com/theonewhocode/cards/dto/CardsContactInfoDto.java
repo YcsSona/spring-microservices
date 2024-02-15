1e9cb84f42e13a7ae29816d13a12d1dbd255aa52
ycssona <ycssona@gmail.com>
2024-02-15 22:57:50 +0530

    Refresh configurations at runtime using refresh actuator path

--- cards/src/main/java/com/theonewhocode/cards/dto/CardsContactInfoDto.java
+++ cards/src/main/java/com/theonewhocode/cards/dto/CardsContactInfoDto.java
@@ -1,10 +1,17 @@
 package com.theonewhocode.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class CardsContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
--- loans/src/main/java/com/theonewhocode/loans/dto/LoansContactInfoDto.java
+++ loans/src/main/java/com/theonewhocode/loans/dto/LoansContactInfoDto.java
@@ -1,10 +1,17 @@
 package com.theonewhocode.loans.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class LoansContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
