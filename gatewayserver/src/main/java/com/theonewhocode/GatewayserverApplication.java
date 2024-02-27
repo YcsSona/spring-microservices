5bc983ab5e485ee9fbe38a11a12d7ded8f10899e
ycssona <ycssona@gmail.com>
2024-02-27 23:26:00 +0530

    gatewayserver: Implementing addResponseHeader filter for all routes

--- gatewayserver/src/main/java/com/theonewhocode/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/theonewhocode/GatewayserverApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class GatewayserverApplication {
 
@@ -18,13 +20,16 @@ public class GatewayserverApplication {
         return routeLocatorBuilder
                 .routes()
                 .route(r -> r.path("/eazybank/accounts/**")
-                        .filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://ACCOUNTS"))
                 .route(r -> r.path("/eazybank/loans/**")
-                        .filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://LOANS"))
                 .route(r -> r.path("/eazybank/cards/**")
-                        .filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://CARDS"))
                 .build();
     }
